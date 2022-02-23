db278c9f6dc921e32ebde73a12677f534e57a92c
naitikvarshney <69815982+naitikvarshney@users.noreply.github.com>
2022-02-24 01:07:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
     );
   return (
     <div className="app">
-        <h1>Crypto Dashboard</h1>
+        <h1 className='heading'>Crypto Dashboard</h1>
         <div className="app-wrapper">
             <CurrencyConverter />
             <NewsFeed />
